8ea8c2d52d487c0fe92d883af5635f9e9304d391
Bilbo Baggins <kravets.sergio@gmail.com>
2022-07-04 13:43:49 +0300

    make it fly

---
+++ app.py
@@ -0,0 +1,100 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask import render_template, url_for, request, redirect
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+ 
+
+
+@app.route('/')
+@app.route('/homepage')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/aboutme')
+def about():
+    return render_template("aboutme.html")
+
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template("posts.html", articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_id(id):
+    article = Article.query.get(id)
+    return render_template("post_detail.html", article=article)
+
+@app.route('/posts/<int:id>/delete')
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try: 
+        db.session.delete()
+        db.session.commit()
+        return redirect('/posts')
+    except: 
+        return "Erron when deleting the article"
+
+@app.route('/posts/<int:id>/edit', methods=['POST', 'GET'])
+def edit_post():
+    article = Article.query.get(id)
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except: 
+            return 'Mistake when adding article'
+    else:
+        return render_template("post_edit.html", article=article)        
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except: 
+            return 'Mistake when adding article'
+    else:
+        return render_template("create-article.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
